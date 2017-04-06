11b4bdbc9d74d3f27b60c77c38845e9862a73da4
tianfei hao <talenhao@gmail.com>
2017-04-06 15:48:00 +0800

    使用generic编写view

--- polls/urls.py
+++ polls/urls.py
@@ -3,8 +3,12 @@ from . import views
 
 app_name = 'polls'
 urlpatterns = [
-    url(r'^$', views.index, name='Home'),
-    url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
-    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^$', views.IndexView.as_view(), name='index'),
+    # generic.DetailView要使用pk的名称，所有相关的url匹配名称都要修改成pk
+    url(r'^specifics/(?P<pk>[0-9]+)/$', views.DetailView.as_view(), name='detail'),
+    url(r'^(?P<pk>[0-9]+)/results/$', views.ResultView.as_view(), name='results'),
+    # url(r'^$', views.index, name='Home'),
+    # url(r'^specifics/(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
     url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
--- polls/views.py
+++ polls/views.py
@@ -4,29 +4,50 @@ from django.template import loader
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 from django.urls import reverse
+from django.views import generic
 
 
 # def index(request):
 #     return HttpResponse('Hello world. You are in the Polls index.')
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[0:5]
-    # output = ','.join([q.question_text for q in latest_question_list])
-    tempalte = loader.get_template('polls/index.html')
-    context = {
-        "latest_question_list": latest_question_list,
-    }
-    # return HttpResponse(tempalte.render(context, request))
-    return render(request, 'polls/index.html', context)
+# def index(request):
+#     latest_question_list = Question.objects.order_by('-pub_date')[0:5]
+#     # output = ','.join([q.question_text for q in latest_question_list])
+#     tempalte = loader.get_template('polls/index.html')
+#     context = {
+#         "latest_question_list": latest_question_list,
+#     }
+#     # return HttpResponse(tempalte.render(context, request))
+#     return render(request, 'polls/index.html', context)
+
+class IndexView(generic.ListView):
+    '''
+    默认使用 <app name>/<model name>_list.html，使用tempate_name修改默认模板
+    默认使用<model name>_list定义model,使用context_object_name修改默认
+    '''
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """return the late five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist.")
-    return render(request, 'polls/detail.html', {'question': question})
-    # return HttpResponse("You're look at question %s." % question_id)
+# def detail(request, question_id):
+#     try:
+#         question = Question.objects.get(pk=question_id)
+#     except Question.DoesNotExist:
+#         raise Http404("Question does not exist.")
+#     return render(request, 'polls/detail.html', {'question': question})
+#     # return HttpResponse("You're look at question %s." % question_id)
+
+
+class DetailView(generic.DetailView):
+    '''
+    默认使用 <app name>/<model name>_detail.html，使用tempate_name手工指定模板.
+    '''
+    model = Question
+    template_name = 'polls/detail.html'
 
 
 def vote(request, question_id):
@@ -45,6 +66,12 @@ def vote(request, question_id):
         return HttpResponseRedirect(reverse("polls:results", args=(question_id,)))
 
 
-def results(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/results.html', {"question": question})
+# def results(request, question_id):
+#     question = get_object_or_404(Question, pk=question_id)
+#     return render(request, 'polls/results.html', {"question": question})
+#
+
+class ResultView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
